54510c1b3231dbf4a07638e43da63999fe6ecb7a
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-09 23:35:37 +0530

    IndianStateCencusAnalyser

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using IndianCensusAnalyzer;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace IndianStateCensusAnalyzer
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            ImportCSVData obj = new ImportCSVData(C: /Users/Shivam Shekhar/source/DAY 29 / IndianStateCensusData.csv");
+            obj.ImportData();
+        }
+    }
+}
